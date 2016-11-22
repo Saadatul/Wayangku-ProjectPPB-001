e4219012a0e21ea1669778fd1257fbcdfc518914
Arik Nur Khoiriyah <ariknurkhoiriyah@gmail.com>
2016-11-22 19:54:38 +0700

    Image Slider

--- app/src/main/java/projectppb001/xirpl605142332/project/smktelkom_mlg/sch/id/wayangku_projectppb_001/NavHome.java
+++ app/src/main/java/projectppb001/xirpl605142332/project/smktelkom_mlg/sch/id/wayangku_projectppb_001/NavHome.java
@@ -1,13 +1,21 @@
 package projectppb001.xirpl605142332.project.smktelkom_mlg.sch.id.wayangku_projectppb_001;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 public class NavHome extends AppCompatActivity {
+
+    ViewPager viewPager;
+    CustomAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav_home);
+        viewPager = (ViewPager)findViewById(R.id.view_pager);
+        adapter = new CustomAdapter(this);
+        viewPager.setAdapter(adapter);
+
     }
 }

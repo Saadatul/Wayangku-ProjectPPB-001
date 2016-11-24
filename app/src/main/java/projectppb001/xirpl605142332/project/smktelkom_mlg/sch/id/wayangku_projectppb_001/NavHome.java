c33b1b4673f096e337d8199aede34f68a15613ed
Arik Nur Khoiriyah <ariknurkhoiriyah@gmail.com>
2016-11-24 19:59:53 +0700

    Add about us

--- app/src/main/java/projectppb001/xirpl605142332/project/smktelkom_mlg/sch/id/wayangku_projectppb_001/NavHome.java
+++ app/src/main/java/projectppb001/xirpl605142332/project/smktelkom_mlg/sch/id/wayangku_projectppb_001/NavHome.java
@@ -3,6 +3,7 @@ package projectppb001.xirpl605142332.project.smktelkom_mlg.sch.id.wayangku_proje
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class NavHome extends AppCompatActivity {
 
@@ -16,6 +17,8 @@ public class NavHome extends AppCompatActivity {
         viewPager = (ViewPager)findViewById(R.id.view_pager);
         adapter = new CustomAdapter(this);
         viewPager.setAdapter(adapter);
+
+
 
     }
 }
